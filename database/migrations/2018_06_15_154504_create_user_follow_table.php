e333ac20716e07865a7ed484518e8636e62dd876
jiro.kirameki <ec2-user@ip-172-31-44-188.us-east-2.compute.internal>
2018-06-20 00:27:22 +0000

    follow, unfollow

---
+++ database/migrations/2018_06_15_154504_create_user_follow_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFollowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follow', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follow');
+    }
+}
